d410345ca2e65c1b7d6e790a4372fca7bb282113
MahadiHasanWeb <muhammad.mahadi.hasan.9@gmail.com>
2023-05-19 20:49:17 +0600

    Add BestToys Extra section

--- src/Components/Home/BestToys.jsx
+++ src/Components/Home/BestToys.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BestToys = () => {
     return (
         <div>
             <div className="relative w-full">
                 <img src='https://i.imgur.com/XrF59ay.jpg' className="w-full rounded-xl" />
-                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                    <div className='text-white space-y-7 pl-12 w-1/2'>
-                        <h2 className='text-6xl font-bold'>Affordable Price For Car Servicing</h2>
-                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                <div className="absolute rounded-xl flex justify-center items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                    <div className='text-white space-y-5 md:space-y-10 px-8 md:px-20 text-center'>
+                        <h2 className='md:text-5xl text-xl font-bold '>We make your children happier with the best toys</h2>
                         <div>
-                            <button className="btn btn-primary mr-5">Discover More</button>
-                            <button className="btn btn-outline btn-secondary">Latest Project</button>
+                            <Link to='/allToys'><button className="button bg-[#57C5B6] mb-8">Explore Now </button></Link>
                         </div>
                     </div>
                 </div>
